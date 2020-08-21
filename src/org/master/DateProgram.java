23597b3cf47b653de043c60bb82e0fa4b9651786
Gowthamdq1 <gowthamdq1@gmail.com>
2020-08-21 19:47:44 +0530

    test project

---
+++ src/org/master/DateProgram.java
@@ -0,0 +1,17 @@
+package org.master;
+
+import java.util.Date;
+
+public class DateProgram {
+	
+	public static void main(String[] args) {
+		Date d=new Date();
+		System.out.println("Today's Date : "+d);
+		System.out.println("Date successfully printed");
+		System.out.println(new Date());
+		
+		
+		
+	}
+
+}
